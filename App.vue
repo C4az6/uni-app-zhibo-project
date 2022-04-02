ea2d1d35cd7a0d93aca86bfc7ec192315bf3cda0
伤心的瘦子 <1762458611@qq.com>
2022-04-03 04:04:05 +0800

    引入字体图标库

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="container"></view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		onLaunch() {
+			const domModule = uni.requireNativePlugin('dom')
+			domModule.addRule('fontFace', {
+				'fontFamily': "iconfont",
+				'src': "url('http://at.alicdn.com/t/font_1859985_7mxozsfdvib.ttf')"
+			});
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("/common/free.css");
+	@import url("/common/common.css");
+	/* #ifndef APP-NVUE */
+	/* 不是NVUE端 */
+	@import url("/common/icon.css");
+	/* #endif */
+</style>
