ba73579b9be36336b72c5f8df13bbda3f8ed03c7
伤心的瘦子 <1762458611@qq.com>
2022-04-03 04:30:35 +0800

    配置tabbar完成

--- App.vue
+++ App.vue
@@ -8,11 +8,17 @@
 			return {}
 		},
 		onLaunch() {
+			// nvue端加载字体图标
 			const domModule = uni.requireNativePlugin('dom')
 			domModule.addRule('fontFace', {
 				'fontFamily': "iconfont",
 				'src': "url('http://at.alicdn.com/t/font_1859985_7mxozsfdvib.ttf')"
 			});
+			
+			// 监听底部导航中间凸起按钮
+			uni.onTabBarMidButtonTap(() => {
+				console.log('点击了中间按钮~')
+			})
 		}
 	}
 </script>
